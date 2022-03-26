ea180e0886df48a9500cb489600e70edd8fe8630
dollarortwo <jleong001@gmail.com>
2022-03-26 15:04:47 -0700

    added function declarations

---
+++ Palindrome.cpp
@@ -0,0 +1,19 @@
+// Test for palindrome property.
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+void swap(char& v1, char& v2);
+// Interchanges the values of v1 and v2.
+
+string reverse(const string& s);
+// Returns a copy of s but with characters in reverse order.
+
+bool is_pal(const string& s);
+// Returns true if s is a palindrome, false otherwise.
+
+int main()
+{
+    
+}
